202b1fb0d58da8c66793489ad0b8d59465d15ebf
Lucas Duarte <lucasduartechaves@hotmail.com>
2022-07-26 21:00:44 -0300

    Adiciona primeira função de atravessar fazendeiro

---
+++ atv4.js
@@ -0,0 +1,10 @@
+const fazendeiro = document.querySelector("#fazendeiro");
+const lobo = document.querySelector("#lobo");
+const carneiro = document.querySelector("#carneiro");
+const alface = document.querySelector("#alface");
+
+fazendeiro.addEventListener("click", clickListener);
+
+function clickListener(event) {
+  margembaixo.appendChild(fazendeiro);
+}
