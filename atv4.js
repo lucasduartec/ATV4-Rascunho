cbf4467cff9db5a8e1c6f2ddd3aacfa932b8fbd3
Lucas Duarte <lucasduartechaves@hotmail.com>
2022-07-26 23:20:49 -0300

    Adiciona desembarque na parte de baixo do rio e corrige na parte de cima

--- atv4.js
+++ atv4.js
@@ -13,43 +13,86 @@ alface.addEventListener("click", moveAlface);
 desce.addEventListener("click", descer);
 
 function moveFazendeiro(event) {
+
+  // Parte de cima
+
   if(fazendeiro.parentElement == margemcima){
     if (cima.childElementCount == 2)
-    return;
+        return;
     else cima.appendChild(fazendeiro);
   }   
-  else margemcima.appendChild(fazendeiro);
+  else if (fazendeiro.parentElement == cima)
+    margemcima.appendChild(fazendeiro);
+
+  //Parte de baixo
+
+  else if (fazendeiro.parentElement == margembaixo)
+            baixo.appendChild(fazendeiro);
+  else if (fazendeiro.parentElement == baixo)
+            margembaixo.appendChild(fazendeiro);
 }
 
 function moveLobo(event) {
+
+  // Parte de cima
+
   if(lobo.parentElement == margemcima){
     if (cima.childElementCount == 2)
     return;
     else cima.appendChild(lobo);
-  }   
-  else margemcima.appendChild(lobo);
+  }
+  else if (lobo.parentElement == cima)
+    margemcima.appendChild(lobo);
+
+  //Parte de baixo
+
+  else if (lobo.parentElement == margembaixo)
+            baixo.appendChild(lobo);
+  else if (lobo.parentElement == baixo)
+  margembaixo.appendChild(lobo);
 }
 
 function moveCarneiro(event) {
+
+  // Parte de cima
+
   if(carneiro.parentElement == margemcima){
     if (cima.childElementCount == 2)
     return;
     else cima.appendChild(carneiro);
-  }   
-  else margemcima.appendChild(carneiro);
+  }
+  else if (carneiro.parentElement == cima)
+    margemcima.appendChild(carneiro);
+
+  //Parte de baixo
+
+  else if (carneiro.parentElement == margembaixo)
+            baixo.appendChild(carneiro);
+  else if (carneiro.parentElement == baixo)
+  margembaixo.appendChild(carneiro);
 }
 
 function moveAlface(event) {
 
+  // Parte de cima
+
   if(alface.parentElement == margemcima){
     if (cima.childElementCount == 2)
-    return;
+      return;
     else cima.appendChild(alface);
-  }   
-  else margemcima.appendChild(alface);
+  }
+  else if (alface.parentElement == cima)
+    margemcima.appendChild(alface);
+
+  //Parte de baixo
+
+  else if (alface.parentElement == margembaixo)
+            baixo.appendChild(alface);
+  else if (alface.parentElement == baixo)
+  margembaixo.appendChild(alface);
 }
 
 function descer(event){
-    const eNovo = cima.children;
-    baixo.appendChild(eNovo[0]);
-} 
+    while (cima.firstChild)
+      baixo.appendChild(cima.firstChild);
+}
